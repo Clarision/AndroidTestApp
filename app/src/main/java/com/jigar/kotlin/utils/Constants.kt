7b68878eba6c29b78389f4fde449b86f2891ab7c
Jigar <jigar2596moradiya@gmail.com>
2020-12-01 13:45:13 +0530

    Submit Code

--- app/src/main/java/com/jigar/kotlin/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.jigar.kotlin.utils
-
-object Constants {
-
-    /** The base URL of the API */
-    const val BASE_URL: String = "http://mclytics.com/api/"
-
-    const val OTP_SIZE = 6
-}
